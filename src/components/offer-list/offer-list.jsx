daa65293643215358d888e37318295f36d33fcb8
Pavel Komarov <komarov.pa.it@gmail.com>
2020-02-29 23:55:59 +0300

    Добавляет состояние и его изменение в OfferList

--- src/components/offer-list/offer-list.jsx
+++ src/components/offer-list/offer-list.jsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import OfferCard from '../offer-card/offer-card.jsx';
 
 
-const handleOfferMouseOver = () => {};
+class OfferList extends PureComponent {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      activeOffer: null,
+    };
 
-const OfferList = ({offers, onOfferTitleClick}) => {
-  return (
-    <div className="cities__places-list places__list tabs__content">
-      {offers.map((offer, i) => (
-        <OfferCard
-          key={`${i}-${offer.title}`}
-          offer={offer}
-          onMouseOver={handleOfferMouseOver}
-          onTitleClick={onOfferTitleClick}
-        />
-      ))}
-    </div>
-  );
-};
+    this._handleOfferCardMouseOver = this._handleOfferCardMouseOver.bind(this);
+  }
+
+  _handleOfferCardMouseOver(offer) {
+    this.setState({
+      activeOffer: offer,
+    });
+  }
+
+  render() {
+    const {offers, onOfferTitleClick} = this.props;
+
+    return (
+      <div className="cities__places-list places__list tabs__content">
+        {offers.map((offer, i) => (
+          <OfferCard
+            key={`${i}-${offer.title}`}
+            offer={offer}
+            onMouseOver={this._handleOfferCardMouseOver}
+            onTitleClick={onOfferTitleClick}
+          />
+        ))}
+      </div>
+    );
+  }
+}
 
 
 OfferList.propTypes = {
