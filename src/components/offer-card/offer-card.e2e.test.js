c848e08bb881c95e84108d5d75f53570aac9360b
Pavel Komarov <komarov.pa.it@gmail.com>
2020-03-01 00:09:07 +0300

    Редактирует и расширяет e2e-тест для OfferCard

--- src/components/offer-card/offer-card.e2e.test.js
+++ src/components/offer-card/offer-card.e2e.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import {OfferType} from '../../const.js';
 import OfferCard from './offer-card.jsx';
 
 
@@ -9,19 +10,47 @@ Enzyme.configure({
 });
 
 
-it(`Should call onCardNameClick`, () => {
-  const onCardNameClick = jest.fn();
+const offer = {
+  picture: ``,
+  isPremium: true,
+  price: 0,
+  title: ``,
+  type: OfferType.APARTMENT,
+  rating: 0,
+};
 
-  const placeCard = shallow(
+
+it(`Should call onTitleClick`, () => {
+  const onTitleClick = jest.fn();
+
+  const offerCard = shallow(
       <OfferCard
-        place={`Some place somewhere`}
-        onCardNameClick={onCardNameClick}
+        offer={offer}
+        onMouseOver={() => {}}
+        onTitleClick={onTitleClick}
       />
   );
 
-  const cardName = placeCard.find(`.place-card__name a`);
+  const offerTitleElement = offerCard.find(`.place-card__name a`);
 
-  cardName.props().onClick();
+  offerTitleElement.props().onClick();
 
-  expect(onCardNameClick).toHaveBeenCalledTimes(1);
+  expect(onTitleClick).toHaveBeenCalled();
+});
+
+it(`Should pass offer to onMouseOver on mouseover event`, () => {
+  const onMouseOver = jest.fn();
+
+  const offerCard = shallow(
+      <OfferCard
+        offer={offer}
+        onMouseOver={onMouseOver}
+        onTitleClick={() => {}}
+      />
+  );
+
+  const offerCardContainerElement = offerCard.find(`.place-card`);
+  offerCardContainerElement.simulate(`mouseover`);
+
+  expect(onMouseOver).toHaveBeenCalledWith(offer);
 });
