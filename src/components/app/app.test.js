9f03b47bbd94ff2b4a7b8792e76d78351f2140cc
Pavel Komarov <komarov.pa.it@gmail.com>
2020-03-01 00:33:50 +0300

    Обновляет оставшиеся snapshot-тесты

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,22 +1,46 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {OfferType} from '../../const.js';
 import App from './app.jsx';
 
 
-const PLACES = [
-  `Beautiful & luxurious apartment at great location`,
-  `Wood and stone place`,
-  `bruh`,
-  `bar`,
-  `fooz`,
-  `baz`,
+const offers = [
+  {
+    picture: `img/apartment-01.jpg`,
+    isPremium: true,
+    price: 120,
+    title: `Beautiful luxurious apartment at great location`,
+    type: OfferType.APARTMENT,
+    rating: 0.1,
+  }, {
+    picture: `img/room.jpg`,
+    isPremium: false,
+    price: 80,
+    title: `Wood and stone place`,
+    type: OfferType.ROOM,
+    rating: 1.3,
+  }, {
+    picture: `img/apartment-02.jpg`,
+    isPremium: true,
+    price: 123,
+    title: `Canal View Prinsengracht`,
+    type: OfferType.HOUSE,
+    rating: 3.6,
+  }, {
+    picture: `img/apartment-03.jpg`,
+    isPremium: false,
+    price: 9999,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: OfferType.HOTEL,
+    rating: 4.87,
+  }
 ];
 
 
 it(`Should match snapshot of App`, () => {
   const tree = renderer
     .create(<App
-      places={PLACES}
+      offers={offers}
     />)
     .toJSON();
 
--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -1,23 +1,47 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {OfferType} from '../../const.js';
 import Main from './main.jsx';
 
 
-const PLACES = [
-  `Beautiful & luxurious apartment at great location`,
-  `Wood and stone place`,
-  `bruh`,
-  `bar`,
-  `fooz`,
-  `baz`,
+const offers = [
+  {
+    picture: `img/apartment-01.jpg`,
+    isPremium: true,
+    price: 120,
+    title: `Beautiful luxurious apartment at great location`,
+    type: OfferType.APARTMENT,
+    rating: 0.1,
+  }, {
+    picture: `img/room.jpg`,
+    isPremium: false,
+    price: 80,
+    title: `Wood and stone place`,
+    type: OfferType.ROOM,
+    rating: 1.3,
+  }, {
+    picture: `img/apartment-02.jpg`,
+    isPremium: true,
+    price: 123,
+    title: `Canal View Prinsengracht`,
+    type: OfferType.HOUSE,
+    rating: 3.6,
+  }, {
+    picture: `img/apartment-03.jpg`,
+    isPremium: false,
+    price: 9999,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: OfferType.HOTEL,
+    rating: 4.87,
+  }
 ];
 
 
 it(`Should match snapshot of Main`, () => {
   const tree = renderer
     .create(<Main
-      places={PLACES}
-      onCardNameClick={() => {}}
+      offers={offers}
+      onOfferTitleClick={() => {}}
     />)
     .toJSON();
 
