a4069b125a89bcd7c49df34daf94f4ee8d410894
Pavel Komarov <komarov.pa.it@gmail.com>
2020-02-29 23:57:02 +0300

    Обновляет snapshot-тест для OfferCard

--- src/components/offer-card/offer-card.test.js
+++ src/components/offer-card/offer-card.test.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {OfferType} from '../../const.js';
 import OfferCard from './offer-card.jsx';
 
 
-it(`Should match snapshot of PlaceCard`, () => {
+const offer = {
+  picture: `img/apartment-01.jpg`,
+  isPremium: true,
+  price: 120,
+  title: `Beautiful luxurious apartment at great location`,
+  type: OfferType.APARTMENT,
+  rating: 0.1,
+};
+
+
+it(`Should match snapshot of OfferCard`, () => {
   const tree = renderer
     .create(<OfferCard
-      place={`Some place somewhere`}
-      onCardNameClick={() => {}}
+      offer={offer}
+      onMouseOver={() => {}}
+      onTitleClick={() => {}}
     />)
     .toJSON();
 
