fc86d434da4cc2a957460eb9bbcfe062c4b756e9
Pavel Komarov <komarov.pa.it@gmail.com>
2020-04-02 19:50:12 +0300

    Добавляет компонент offer, описывающий страницу с подробным описанием предложения

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from '../main/main.jsx';
+import Offer from '../offer/offer.jsx';
 
 
 const handleOfferTitleClick = () => {};
 
 
-const App = ({offers}) => {
+const renderApp = (offers) => {
   return (
     <Main
       offers={offers}
       onOfferTitleClick={handleOfferTitleClick}
     />
+  );
+};
+
+const App = ({offers}) => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          {renderApp(offers)}
+        </Route>
+        <Route exact path="/dev-main">
+          <Main
+            offers={offers}
+            onOfferTitleClick={handleOfferTitleClick}
+          />
+        </Route>
+        <Route exact path="/dev-offer">
+          <Offer/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
