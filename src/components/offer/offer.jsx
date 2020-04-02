90c4475c58edd848d548160704cc9376cba5aefa
Pavel Komarov <komarov.pa.it@gmail.com>
2020-04-02 23:36:49 +0300

    Заставляет Offer рисоваться из данных, полученных в пропсах (пока без отзывов и предложений неподалёку)

--- src/components/offer/offer.jsx
+++ src/components/offer/offer.jsx
@@ -2,19 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
+const MAX_IMAGE_COUNT = 6;
+
+
+const convertStarRatingToPercentageRating = (starRating) => Math.round(starRating) * 20;
+
+
 const Offer = ({
-  images,
-  title,
-  description,
-  isPremium,
-  type,
-  rating,
-  bedroomCount,
-  maxGuestCount,
-  price,
-  features,
-  host,
+  offer: {
+    images,
+    isPremium,
+    title,
+    rating,
+    type,
+    bedroomCount,
+    maxGuestCount,
+    price,
+    features,
+    host: {
+      avatar,
+      name,
+      isSuper
+    },
+    description,
+  }
 }) => {
+  const percentageRating = convertStarRatingToPercentageRating(rating);
+
   return (
     <div className="page">
       <header className="header">
@@ -44,34 +58,23 @@ const Offer = ({
         <section className="property">
           <div className="property__gallery-container container">
             <div className="property__gallery">
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/room.jpg" alt="Photo studio"/>
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio"/>
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-02.jpg" alt="Photo studio"/>
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-03.jpg" alt="Photo studio"/>
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/studio-01.jpg" alt="Photo studio"/>
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio"/>
-              </div>
+              {images.slice(0, MAX_IMAGE_COUNT).map((image, i) => (
+                <div key={`${i}-${image}`} className="property__image-wrapper">
+                  <img className="property__image" src={image} alt="Photo studio"/>
+                </div>
+              ))}
             </div>
           </div>
           <div className="property__container container">
             <div className="property__wrapper">
-              <div className="property__mark">
-                <span>Premium</span>
-              </div>
+              {isPremium &&
+                <div className="property__mark">
+                  <span>Premium</span>
+                </div>
+              }
               <div className="property__name-wrapper">
                 <h1 className="property__name">
-                  Beautiful &amp; luxurious studio at great location
+                  {title}
                 </h1>
                 <button className="property__bookmark-button button" type="button">
                   <svg className="property__bookmark-icon" width="31" height="33">
@@ -82,77 +85,53 @@ const Offer = ({
               </div>
               <div className="property__rating rating">
                 <div className="property__stars rating__stars">
-                  <span style={{width: `80%`}}></span>
+                  <span style={{width: `${percentageRating}%`}}></span>
                   <span className="visually-hidden">Rating</span>
                 </div>
-                <span className="property__rating-value rating__value">4.8</span>
+                <span className="property__rating-value rating__value">
+                  {rating}
+                </span>
               </div>
               <ul className="property__features">
                 <li className="property__feature property__feature--entire">
-                  Apartment
+                  {type}
                 </li>
                 <li className="property__feature property__feature--bedrooms">
-                  3 Bedrooms
+                  {bedroomCount} Bedrooms
                 </li>
                 <li className="property__feature property__feature--adults">
-                  Max 4 adults
+                  Max {maxGuestCount} adult{maxGuestCount > 1 ? `s` : ``}
                 </li>
               </ul>
               <div className="property__price">
-                <b className="property__price-value">&euro;120</b>
+                <b className="property__price-value">
+                  &euro;{price}
+                </b>
                 <span className="property__price-text">&nbsp;night</span>
               </div>
               <div className="property__inside">
                 <h2 className="property__inside-title">What&apos;s inside</h2>
                 <ul className="property__inside-list">
-                  <li className="property__inside-item">
-                    Wi-Fi
-                  </li>
-                  <li className="property__inside-item">
-                    Washing machine
-                  </li>
-                  <li className="property__inside-item">
-                    Towels
-                  </li>
-                  <li className="property__inside-item">
-                    Heating
-                  </li>
-                  <li className="property__inside-item">
-                    Coffee machine
-                  </li>
-                  <li className="property__inside-item">
-                    Baby seat
-                  </li>
-                  <li className="property__inside-item">
-                    Kitchen
-                  </li>
-                  <li className="property__inside-item">
-                    Dishwasher
-                  </li>
-                  <li className="property__inside-item">
-                    Cabel TV
-                  </li>
-                  <li className="property__inside-item">
-                    Fridge
-                  </li>
+                  {features.map((feature, i) => (
+                    <li key={`${i}-${feature}`} className="property__inside-item">
+                      {feature}
+                    </li>
+                  ))}
                 </ul>
               </div>
               <div className="property__host">
                 <h2 className="property__host-title">Meet the host</h2>
                 <div className="property__host-user user">
-                  <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                    <img className="property__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar"/>
+                  <div className={`property__avatar-wrapper ${isSuper ? `property__avatar-wrapper--pro ` : ``}user__avatar-wrapper`}>
+                    <img className="property__avatar user__avatar" src={avatar} width="74" height="74" alt="Host avatar"/>
                   </div>
                   <span className="property__user-name">
-                    Angelina
+                    {name}
                   </span>
                 </div>
                 <div className="property__description">
                   <p className="property__text">
-                    A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                  </p>
-                  <p className="property__text">
-                    An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.
+                    {description}
                   </p>
                 </div>
               </div>
@@ -342,20 +321,22 @@ const Offer = ({
 
 
 Offer.propTypes = {
-  images: PropTypes.arrayOf(PropTypes.string).isRequired,
-  title: PropTypes.string.isRequire,
-  description: PropTypes.string.isRequired,
-  isPremium: PropTypes.bool.isRequired,
-  type: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-  bedroomCount: PropTypes.number.isRequired,
-  maxGuestCount: PropTypes.number.isRequired,
-  price: PropTypes.number.isRequired,
-  features: PropTypes.arrayOf(PropTypes.string).isRequired,
-  host: PropTypes.shape({
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    isSuper: PropTypes.bool.isRequired,
+  offer: PropTypes.shape({
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    isPremium: PropTypes.bool.isRequired,
+    title: PropTypes.string.isRequire,
+    rating: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
+    bedroomCount: PropTypes.number.isRequired,
+    maxGuestCount: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+    features: PropTypes.arrayOf(PropTypes.string).isRequired,
+    host: PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isSuper: PropTypes.bool.isRequired,
+    }).isRequired,
+    description: PropTypes.string.isRequired,
   }).isRequired,
 };
 
